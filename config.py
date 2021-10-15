ff92968246c3efbe1cff3d0a951038d9eaa83d4f
Seng YANG <91038988+Seng555@users.noreply.github.com>
2021-10-15 19:05:19 +0700

    Udate Nwe UI

--- config.py
+++ config.py
@@ -2,16 +2,16 @@
 walletAddress = "0x0000000000000000000000000000000000000000"                     #Your Wall address From trustwallet or another wallet.
 private_key = "0x00000000000000000000000000000000000000000000000000000000000000" #Wallet private_key
 
-spend = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"  # WBNB OR OTHER contract for buy the token
+spend = "0xe9e7cea3dedca5984780bafc599bd69add087d56"  # WBNB OR OTHER contract for buy the token
 
-AmountForSnipe = 0.0010  # Amount how much you want buy the token in WBNB OR OTHER.
+AmountForSnipe = 0.001  # Amount how much you want buy the token in WBNB OR OTHER.
 MinLiquidityAdded = 10  # Set how much minimum liquidity added in pair address that you want to buy. set in WBNB OR OTHER. (eg : 2, 4, 7). 2 mean 2 BNB liquidity added.
 MaxSlippage = 25  # Max Slippage or Prince Impact
-SellToken = 1   # 0 = Not Sell after buy, 1 = Sell token after buy by take profit
-Takeprofit = 250 # On percent
+SellToken = 0   # 0 = Not Sell after buy, 1 = Sell token after buy by take profit
+Takeprofit = 300 # On percent
 
 transactionRevertTime = 1000 #Limit for make transaction
-gasAmount = 600000 #Minimul limit is 210000, more much more better.
+gasAmount = 500000 #Minimul limit is 210000, more much more better.
 gasPrice = 7 #Customize your GWEI (gas fee) here, cannot decimal. (eg : 5, 10, 25).
 
 bscScanAPIKey= "NYBZQ1DI6FMBTQRJBV9YKRXAJHGY1N2HYE" #Your BSC API Key Get From https://bscscan.com
